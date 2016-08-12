c307dd599cc2d27b3c602a7e8bcee03295441805
Lopez, Mauricio (ml5174) <ml5174@att.com>
2016-08-12 12:23:53 -0400

    first firebase commit

--- app/app.ts
+++ app/app.ts
@@ -4,6 +4,8 @@ import {StatusBar} from 'ionic-native';
 import {TabsPage} from './pages/tabs/tabs';
 import {ListPage} from './pages/list/list';
 import {LogonPage} from './pages/logon/logon';
+import * as firebase from 'firebase';
+import {FIREBASE_PROVIDERS, AuthProviders, AuthMethods, AngularFire} from 'angularfire2'
 
 @Component({
   templateUrl: 'build/app.html'
@@ -13,11 +15,12 @@ class MyApp {
 
   // make HelloIonicPage the root (or first) page
   rootPage: any = TabsPage;
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any }>;
 
   constructor(
     public platform: Platform,
-    public menu: MenuController
+    public menu: MenuController,
+    public af: AngularFire
   ) {
     this.initializeApp();
 
@@ -30,6 +33,20 @@ class MyApp {
   }
 
   initializeApp() {
+    const
+      fbConf = {
+        apiKey: "AIzaSyA0K2ZncwedgeS8enAzBx89p-8WxPecvrU",
+        authDomain: "sacpcp-89085.firebaseapp.com",
+        databaseURL: "https://sacpcp-89085.firebaseio.com",
+        storageBucket: "sacpcp-89085.appspot.com",
+        provider: AuthProviders.Google,
+        method: AuthMethods.Redirect
+      };
+    const myFirebaseAuthConfig = {
+      provider: AuthProviders.Google,
+      method: AuthMethods.Redirect
+    }
+    firebase.initializeApp(fbConf);
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -42,6 +59,9 @@ class MyApp {
     this.menu.close();
     // navigate to the new page if it is not the current page
     this.nav.setRoot(page.component);
+  }
+   login() {
+    this.af.auth.login();
   }
 }
 
